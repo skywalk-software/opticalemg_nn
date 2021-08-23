b011c51b1f7ecc10a53a2e6b3f5f475618030e0b
Jackie Yang <jackie@jackieyang.me>
2021-08-22 19:28:51 -0700

    v2 with stable all losses

--- main.py
+++ main.py
@@ -172,23 +172,23 @@ class NnModel(pl.LightningModule):
         self.learning_rate = learning_rate
         self.gamma = gamma
         self.mlp_1 = nn.Sequential(
-            nn.Linear(in_features=64, out_features=16),
+            nn.Linear(in_features=64, out_features=48),
             nn.ReLU()
         )
         self.mlp_2 = nn.Sequential(
-            nn.Linear(in_features=16, out_features=16),
+            nn.Linear(in_features=48, out_features=32),
             nn.ReLU()
         )
         self.mlp_3 = nn.Sequential(
-            nn.Linear(in_features=16, out_features=16),
+            nn.Linear(in_features=32, out_features=24),
             nn.ReLU()
         )
         self.mlp_4 = nn.Sequential(
-            nn.Linear(in_features=16, out_features=16),
+            nn.Linear(in_features=24, out_features=12),
             nn.ReLU()
         )
         self.mlp_5 = nn.Sequential(
-            nn.Linear(in_features=16, out_features=4)
+            nn.Linear(in_features=12, out_features=4)
         )
         self.loss = nn.MSELoss(reduction='none')
 
@@ -214,13 +214,13 @@ class NnModel(pl.LightningModule):
         out = out.unsqueeze(1).repeat(1, y.shape[1], 1)
         loss = torch.mean(self.loss_fn(out, y[:, :, 1:]), dim=0)
         pitch_loss, yaw_loss, thumb_dist_loss, other_dist_loss = torch.min(loss, dim=0)[0]
-        total_loss = pitch_loss + yaw_loss + thumb_dist_loss + other_dist_loss
+        total_loss = pitch_loss + yaw_loss + thumb_dist_loss / 1000 + other_dist_loss / 10000
         self.log("train/loss", total_loss)
         self.log("train/pitch_loss", pitch_loss)
         self.log("train/yaw_loss", yaw_loss)
         self.log("train/thumb_dist_loss", thumb_dist_loss)
         self.log("train/other_dist_loss", other_dist_loss)
-        return pitch_loss
+        return total_loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
@@ -228,7 +228,7 @@ class NnModel(pl.LightningModule):
         out = out.unsqueeze(1).repeat(1, y.shape[1], 1)
         loss = torch.mean(self.loss_fn(out, y[:, :, 1:]), dim=0)
         pitch_loss, yaw_loss, thumb_dist_loss, other_dist_loss = torch.min(loss, dim=0)[0]
-        total_loss = pitch_loss + yaw_loss + thumb_dist_loss + other_dist_loss
+        total_loss = pitch_loss + yaw_loss + thumb_dist_loss / 1000 + other_dist_loss / 10000
         self.log("val/loss", total_loss)
         self.log("val/pitch_loss", pitch_loss)
         self.log("val/yaw_loss", yaw_loss)
